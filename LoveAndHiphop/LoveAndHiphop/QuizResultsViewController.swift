06c30d2ab6f59035ec4d967482c632b00430a451
hollywoodno <hollywoodno@gmail.com>
2017-05-12 11:38:26 -0700

    implement grade quiz

--- LoveAndHiphop/LoveAndHiphop/QuizResultsViewController.swift
+++ LoveAndHiphop/LoveAndHiphop/QuizResultsViewController.swift
@@ -7,30 +7,40 @@
 //
 
 import UIKit
+import Parse
 
 class QuizResultsViewController: UIViewController {
-
+  @IBOutlet weak var downArrowImageView: UIImageView!
+  @IBOutlet weak var loginButton: UIButton!
   var resultsView: UIView!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    loginButton.layer.cornerRadius = 4
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  @IBAction func onDownArrowTap(_ sender: UITapGestureRecognizer) {
+    let fbVC = FBViewController(nibName: "FBViewController", bundle: nil)
+    show(fbVC, sender: nil)
+  }
+  
+  @IBAction func onClose(_ sender: Any) {
+    dismiss(animated: true, completion: nil)
+  }
+  
+  /*
+   // MARK: - Navigation
+   
+   // In a storyboard-based application, you will often want to do a little preparation before navigation
+   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+   // Get the new view controller using segue.destinationViewController.
+   // Pass the selected object to the new view controller.
+   }
+   */
+  
 }
